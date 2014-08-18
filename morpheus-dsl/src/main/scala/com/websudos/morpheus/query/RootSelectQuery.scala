3f7a1a817e4433087e9401bad48d8bf63bd23533
Ben Edwards <edwards.benj@gmail.com>
2014-08-18 20:54:00 +0100

    Refactor duplicate definitions
    Moved the join definition body into an auxillary helper function, the
    inline should mean that we end up with exactly what we had before in
    terms of code.

--- morpheus-dsl/src/main/scala/com/websudos/morpheus/query/RootSelectQuery.scala
+++ morpheus-dsl/src/main/scala/com/websudos/morpheus/query/RootSelectQuery.scala
@@ -135,47 +135,33 @@ class SelectQuery[T <: Table[T, _],
     )
   }
 
-  final def innerJoin[Owner <: Table[Owner, Record], Record](join: Table[Owner, Record]): OnJoinQuery[T, (R, Record), SelectType, Group, Order, Limit, Chain,
-    AssignChain, Unterminated] = {
+  @inline
+  private[this] def joinBuilder[Owner <: Table[Owner, Record], Record](
+      joiner: (SQLBuiltQuery, String) => SQLBuiltQuery,
+      join: Table[Owner, Record]): OnJoinQuery[T, (R, Record), SelectType, Group, Order, Limit, Chain, AssignChain, Unterminated] = {
 
     def fromRow(row: Row): (R, Record) = (query.fromRow(row), join.fromRow(row))
 
     new OnJoinQuery(
       new Query(
         query.table,
-        query.table.queryBuilder.innerJoin(query.query, join.tableName),
+        joiner(query.query, join.tableName),
         fromRow
       )
     )
   }
 
-  final def leftJoin[Owner <: Table[Owner, Record], Record](join: Table[Owner, Record]): OnJoinQuery[T, (R, Record), SelectType, Group, Order, Limit, Chain,
-    AssignChain, Unterminated] = {
+  def innerJoin[Owner <: Table[Owner, Record], Record](join: Table[Owner, Record])
+      : OnJoinQuery[T, (R, Record), SelectType, Group, Order, Limit, Chain, AssignChain, Unterminated] =
+    joinBuilder(query.table.queryBuilder.innerJoin, join)
 
-    def fromRow(row: Row): (R, Record) = (query.fromRow(row), join.fromRow(row))
+  def leftJoin[Owner <: Table[Owner, Record], Record](join: Table[Owner, Record])
+      : OnJoinQuery[T, (R, Record), SelectType, Group, Order, Limit, Chain, AssignChain, Unterminated] =
+    joinBuilder(query.table.queryBuilder.leftJoin, join)
 
-    new OnJoinQuery(
-      new Query(
-        query.table,
-        query.table.queryBuilder.leftJoin(query.query, join.tableName),
-        fromRow
-      )
-    )
-  }
-
-  final def rightJoin[Owner <: Table[Owner, Record], Record](join: Table[Owner, Record]): OnJoinQuery[T, (R, Record), SelectType, Group, Order, Limit, Chain,
-    AssignChain, Unterminated] = {
-
-    def fromRow(row: Row): (R, Record) = (query.fromRow(row), join.fromRow(row))
-
-    new OnJoinQuery(
-      new Query(
-        query.table,
-        query.table.queryBuilder.rightJoin(query.query, join.tableName),
-        fromRow
-      )
-    )
-  }
+  def rightJoin[Owner <: Table[Owner, Record], Record](join: Table[Owner, Record])
+      : OnJoinQuery[T, (R, Record), SelectType, Group, Order, Limit, Chain, AssignChain, Unterminated] =
+    joinBuilder(query.table.queryBuilder.rightJoin, join)
 
   private[morpheus] final def terminate: Query[T, R, SelectType, Group, Order, Limit, Chain, AssignChain, Terminated] = {
     new Query(
